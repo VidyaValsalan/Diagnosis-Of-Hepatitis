4c68862bc1627370df648b4002b842c473cc0a4f
VidyaValsalan <55728779+VidyaValsalan@users.noreply.github.com>
2020-05-20 12:40:45 +0530

    Update SVM&RandomForest.py

--- SVM&RandomForest.py
+++ SVM&RandomForest.py
@@ -17,54 +17,41 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 
 # In[19]:
 
-
 cell_df = pd.read_csv('C:\datamining\hepatitis.csv',na_values = '?',names=['Class','AGE','SEX','STEROID','ANTIVIRALS','FATIGUE','MALAISE','ANOREXIA','LIVER BIG','LIVER FIRM','SPLEEN PALPABLE','SPIDERS','ASCITES','VARICES','BILIRUBIN','ALK PHOSPHATE','SGOT','ALBUMIN','PROTIME','HISTOLOGY'])
-
 
 # # Data Cleaning
 
 # In[20]:
 
-
 #Plotshowing the missing value
 sns.heatmap(cell_df.isnull(),yticklabels=False,cbar=False,cmap='viridis')
 
-
 # In[21]:
-
 
 #Ignore the attribute PROMITE contains several tuples with missing values.
 del cell_df['PROTIME']
 #Ignore row 56 contains several attributes with missing values.
 cell_df.drop([56],axis=0,inplace=True)
 
-
 # # Use a measure of central tendency for the attribute (e.g., the mean or median) to fill in the missing value
 
 # In[22]:
-
 
 # For normal (symmetric) data distributions, the mean can be used, while skewed data distribution should employ the median
 # Graph showing the attribute ALK PHOSPHATE is left skewed
 sns.distplot(cell_df['ALK PHOSPHATE'].dropna(),kde=False,color='darkred',bins=40)
 
-
 # In[23]:
-
 
 #Replace with median
 cell_df['ALK PHOSPHATE'].fillna((cell_df['ALK PHOSPHATE'].median()), inplace=True)
 
-
 # In[24]:
-
 
 # Graph showing the attribute ALBUMIN is normally distributed
 sns.distplot(cell_df['ALBUMIN'].dropna(),kde=False,color='darkred',bins=40)
 
-
 # In[25]:
-
 
 #missing value of ALBUMIN replace with mean value
 cell_df['ALBUMIN'].fillna((cell_df['ALBUMIN'].mean()), inplace=True)
@@ -87,52 +74,36 @@ cell_df['BILIRUBIN'].fillna((cell_df['BILIRUBIN'].median()), inplace=True)
 #replace SGOT with median
 cell_df['SGOT'].fillna((cell_df['SGOT'].median()), inplace=True)
 
-
 # In[26]:
-
 
 #Plot showing there is no missing value
 sns.heatmap(cell_df.isnull(),yticklabels=False,cbar=False,cmap='viridis')
 
 
 # In[27]:
-
-
 #Check all the types are numerical or not
 cell_df.dtypes
 
-
 # In[28]:
-
-
 ft_df=cell_df[['AGE','SEX','STEROID','ANTIVIRALS','FATIGUE','MALAISE','ANOREXIA','LIVER BIG','LIVER FIRM','SPLEEN PALPABLE','SPIDERS','ASCITES','VARICES','BILIRUBIN','ALK PHOSPHATE','SGOT','ALBUMIN','HISTOLOGY']]
 x=np.asarray(ft_df)
 y=np.asarray(cell_df['Class'])
 
-
 # In[29]:
-
-
 # For testing purpose, divide the data into the 80/20 training set and testing set
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=4)
 
-
 # # SVM Model
 
 # In[30]:
-
-
 from sklearn import svm
 classifier=svm.SVC(kernel='linear',gamma='auto',C=2)
 classifier.fit(x_train,y_train)
 y_predict=classifier.predict(x_test)
 #y_predict
 
-
 # In[31]:
-
-
 #Checking the result
 from sklearn.metrics import classification_report
 print(classification_report(y_test,y_predict))
@@ -141,8 +112,6 @@ print(classification_report(y_test,y_predict))
 # # Random Forest Model
 
 # In[32]:
-
-
 from sklearn.ensemble import RandomForestClassifier
 rf=RandomForestClassifier(n_estimators=100)
 rf.fit(x_train,y_train)
@@ -150,21 +119,6 @@ y1_predict=rf.predict(x_test)
 
 
 # In[15]:
-
-
 #Checking the result
 from sklearn.metrics import classification_report
 print(classification_report(y_test,y1_predict))
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
